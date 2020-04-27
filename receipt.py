1fd5ae2020fa4691bf3885636e6999d2d7610a08
Diana Kowaski <diana.anna.kowalski@gmail.com>
2020-04-27 15:52:44 -0500

    changed docstrings to NumPy style

--- receipt.py
+++ receipt.py
@@ -4,20 +4,35 @@ import datetime
 class Item:
     """This is a class for products on a receipt.
 
-    :param name: The name/description of the item.
-    :type name: string
-    :param amount: The amount listed for the product on the receipt.
-    :type amount: float
-    :param tax_percent: The tax percent for that particular item.
-    :type tax_percent: float
-    :param is_tax_included: Whether the tax is already included in the amount
-    :type is_tax_included: boolean, optional
-    :param quantity: How many of the same items were purchased.
-    :type: int, optional
+    Attributes
+    ----------
+        name : str
+            The name/description of the item.
+        amount : float
+            The amount listed for the product on the receipt.
+        tax_percent : float
+            The tax percent for that particular item.
+        is_tax_included : bool
+            Whether the tax is already included in the amount.
+        quantity : int
+            How many of the same items were purchased.
     """
 
     def __init__(self, name, amount, tax_percent, is_tax_included=False, quantity=1):
         """The constructor method.
+
+        Parameters
+        ----------
+        name : str
+            The name/description of the item.
+        amount : float
+            The amount listed for the item on the receipt.
+        tax_percent : float
+            The tax percent for that particular item. Write 22 not 0.22.
+        is_tax_included : bool, optional
+            Whether the tax is already included in the amount.
+        quantity : int
+            How many of the same items were purchased.
         """
         self.name = name
         self.is_tax_included = is_tax_included
@@ -37,28 +52,40 @@ class Receipt:
     """
     This is a class to represent receipts.
 
-    :param purchase_date: The date printed on the receipt.
-    :type: datetime
-    :param store: The store where the purchase was made.
-    :type: string
-    :param items: The list of Items which were bought.
-    :type: list
-    :param total: The total amount spent at the store.
-    :type: float, optional
+    purchase_date : datetime
+        The date printed on the receipt.
+    store : str
+        The store where the purchase was made.
+    items : :obj:`list` of :obj:`Item`
+        The list of Items which were bought.
+    total : float
+        The total amount spent at the store.
     """
 
-    def __init__(self, purchase_date, store, total=0):
+    def __init__(self, purchase_date, store, total):
+        """The constructor method.
+
+        Parameters
+        ----------
+        purchase_date : datetime
+            The date when the receipt was issued.
+        store : str
+            The name of the store that issued the receipt.
+        total : float
+            The total amount paid.
+        """
         self.purchase_date = purchase_date
         self.store = store
         self.items = []
         self.total = total
 
     def add_item(self, entry):
-        """
-        Add an item to the receipt.
+        """Adds an item to the receipt.
 
-        :param entry: The entry which you wish to add to the receipt.
-        :type entry: Item
+        Parameters
+        ----------
+        entry : Item
+            The item to be added to the receipt.
         """
         self.items.append(entry)
 
